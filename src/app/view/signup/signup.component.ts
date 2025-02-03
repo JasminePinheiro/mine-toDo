0d76ec946f775323deac53a46a32e5e3e36baee5
Jasmine Pinheiro <jasminepinheiro14@gmail.com>
2025-02-02 23:32:00 -0300

    feat: sign-up page

--- src/app/view/signup/signup.component.ts
+++ src/app/view/signup/signup.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { LibHeaderComponent } from '../../core/shared/header/header.component';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, LibHeaderComponent],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
