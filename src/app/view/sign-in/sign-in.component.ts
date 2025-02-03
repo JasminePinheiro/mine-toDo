b62d418ac9de9854327b3445e733d96cb6b06d7a
Jasmine Pinheiro <jasminepinheiro14@gmail.com>
2025-02-03 01:01:53 -0300

    feat: add routerLink in th page signIn

--- src/app/view/sign-in/sign-in.component.ts
+++ src/app/view/sign-in/sign-in.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { LibHeaderComponent } from '../../core/shared/header/header.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [LibHeaderComponent],
+  imports: [LibHeaderComponent, RouterLink],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss'
 })
